eb4a7a201d20ddcf2d695357cdd56fc18e9ece9e
Levchenko Vladislav <levchenko.vg@gmail.com>
2022-12-26 01:53:56 +0300

    Правка ДЗ 4

--- src/Lesson_4.java
+++ src/Lesson_4.java
@@ -139,12 +139,12 @@ public class Lesson_4 {
             for (int[] step : steps) {
                 int newRow = curRow + step[0];
                 int newCol = curCol + step[1];
-                if (newRow < 0 || newRow >= mat.length ||
-                        newCol < 0 || newCol >= mat[0].length ||
-                        mat[newRow][newCol] <= mat[curPoint[0]][curPoint[1]] + 1) continue;
-                mat[newRow][newCol] = mat[curRow][curCol] + 1;
-                queue.add(new int[]{newRow, newCol});
-
+                if (newRow >= 0 && newRow < mat.length &&
+                        newCol >= 0 && newCol < mat[0].length &&
+                        mat[newRow][newCol] > mat[curRow][curCol] + 1) {
+                    mat[newRow][newCol] = mat[curRow][curCol] + 1;
+                    queue.add(new int[]{newRow, newCol});
+                }
             }
         }
         return mat;
